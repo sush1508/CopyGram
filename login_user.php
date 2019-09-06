326445b4d913cfaf817a8b0fec82f0c3e0b9f8f7
sushant_said <53361883+sush1508@users.noreply.github.com>
2019-09-06 21:56:18 +0530

    Update login_user.php

--- login_user.php
+++ login_user.php
@@ -9,8 +9,9 @@ $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass,$db_name);
-	//mysqli_select_db($conn,"pccoerelearning");
 $user_email = $input['email'];
+$user_name = $input['username'];
+$user_mobile = $input['mobile'];
 $user_password = $input['password'];
 
 
@@ -21,24 +22,19 @@ if (!$conn )
 }
 else
 {
-		$query = "SELECT * FROM  registration WHERE email = '$user_email' AND password ='$user_password'";		
-		$retval = mysqli_query($conn,$query);
+		$query = "INSERT INTO registration VALUES ('$user_name','$user_email','$user_password','$user_mobile')";		
+		$result = mysqli_query($conn,$query);
 		
 		
-		if(mysqli_num_rows($retval)<=0)
+		if(!$result)
 		{
 			$response["status"] = 0;
-			$response["message"] = "Email or passWord is incorrect";
+			$response["message"] = "Error registering the user";
 		}
 		else
 		{
-		    while($row = mysqli_fetch_assoc($retval)) 
-			{ 
-			    $username=$row['username'];
-			}
 			$response["status"] = 1;
-			$response["username"] = $username;
-			$response["message"] = "Login successful";
+			$response["message"] = "Registration successful";
 			
 		}				
 				mysqli_close($conn);
