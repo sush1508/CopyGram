a9252a69b1159242c8d383b292997bbb701e5f65
sushant_said <53361883+sush1508@users.noreply.github.com>
2019-09-06 21:57:46 +0530

    Update save_file.php

--- save_file.php
+++ save_file.php
@@ -1,9 +1,64 @@
-<?php 
-						$file_path = "images/";
-	$file_path = $file_path . basename($_FILES['uploaded_file']['name']);
-	if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path))	{
-		echo "success";
-	}else {
-		echo "error";
+<?php
+
+if(isset($_FILES['uploaded_file']['name']))
+{
+	$file_path = "uploads/";
+	$filename = basename($_FILES['uploaded_file']['name']);
+	$file_path = $file_path . basename(($_FILES['uploaded_file']['name']));
+
+	
+	$filetype = $_FILES['uploaded_file']['type'];
+	$filesize = $_POST['fsize'];
+	$user_email = $_POST['useremail'];
+	$status=0;
+	
+	//check file size
+	if($filesize>5000000000)
+	{
+		$status = 1;
+		echo "file too large";
 	}
+
+
+	if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path))	
+	{
+		
+				$db_name = 'xeroxapp';
+				$db_user = 'root';
+				$db_pass = 'root';
+				$db_host = 'localhost'; 
+			 
+			
+				$conn = mysqli_connect($db_host, $db_user, $db_pass,$db_name);
+				//mysqli_select_db($conn,"pccoerelearning");
+			
+
+				if (!$conn ) 
+				{
+			    		die("Connection failed: " . $conn->connect_error);
+			
+				}
+				else
+				{
+					$query = "insert into documents values('$user_email','$filename','$filetype','$file_path')";		
+					//$retval = mysqli_query($conn,$query);
+						
+					if($conn->query($query) === TRUE  )
+					{
+						
+						$result= "The file ". basename( $_FILES["uploaded_file"]["name"]). " has been uploaded.";
+						//echo $result;	
+					}
+					
+				}
+				mysqli_close($conn);
+				
+
+	}
+	
+
+
+}
+
+	
  ?>
